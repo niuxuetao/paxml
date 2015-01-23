6eda292986e64792afd756e198b8a9b356d8b233
niuxuetao <xuetao.niu@gmail.com>
2015-01-23 17:17:30 +0100

    Add spring boot support

---
+++ PaxmlExecutable/src/main/java/org/paxml/Application.java
@@ -0,0 +1,30 @@
+/**
+ * This file is part of PaxmlExecutable.
+ *
+ * PaxmlExecutable is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PaxmlExecutable is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PaxmlExecutable.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.paxml;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@ComponentScan
+public class Application {
+	public static void main(String[] args) throws Exception{
+		System.out.println("Launched!");
+	}
+}
