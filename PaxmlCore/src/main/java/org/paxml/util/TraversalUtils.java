f136611c3cfe8ad1489d2ff71cbefddadf1972e7
niuxuetao <xuetao.niu@gmail.com>
2015-02-18 22:25:53 +0100

    add more doc

--- PaxmlCore/src/main/java/org/paxml/util/TraversalUtils.java
+++ PaxmlCore/src/main/java/org/paxml/util/TraversalUtils.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of PaxmlCore.
+ *
+ * PaxmlCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PaxmlCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PaxmlCore.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.paxml.util;
 
 import java.util.ArrayList;
@@ -5,6 +21,12 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The utils for traversing collections.
+ * 
+ * @author Xuetao Niu
+ * 
+ */
 public class TraversalUtils {
 	/**
 	 * Make a list of maps out of a map of lists. The given map is treated as a
