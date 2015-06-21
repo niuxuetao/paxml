539947e8b1f06dbb3f5ab33b9c9511ea3c84c75a
niuxuetao <xuetao.niu@gmail.com>
2015-06-22 00:08:41 +0200

    continue refactor

---
+++ PaxmlCore/src/main/java/org/paxml/control/AbstractControlTagFactory.java
@@ -0,0 +1,12 @@
+package org.paxml.control;
+
+import org.paxml.tag.DefaultTagFactory;
+/**
+ * Root class for all control tag factories.
+ * @author Xuetao Niu
+ *
+ * @param <T> a control tag type
+ */
+public abstract class AbstractControlTagFactory<T extends AbstractControlTag> extends DefaultTagFactory <T>{
+
+}
