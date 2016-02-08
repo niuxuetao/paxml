8e333f9b935e9340f5d6b58f3d74455cdbb5f78b
Xuetao Niu <xuetao.niu@NLHFD-LT1031.Corp.irdeto.com>
2016-02-08 11:33:52 +0100

    add more testing, pre release

--- PaxmlCore/src/test/java/org/paxml/util/PaxmlUtilTest.java
+++ PaxmlCore/src/test/java/org/paxml/util/PaxmlUtilTest.java
@@ -1,3 +1,19 @@
+/**
+ * This file is part of PaxmlCore.
+ *
+ * PaxmlCore is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * PaxmlCore is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with PaxmlCore.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.paxml.util;
 
 import java.util.regex.Pattern;
@@ -9,6 +25,7 @@ import junit.framework.Assert;
 public class PaxmlUtilTest {
 	@Test
 	public void testGlobTranslate(){
+		testMatch("*","", true);
 		testMatch("*","blabla", true);
 		testMatch("a*c","asdifksdfkhc", true);
 		testMatch("a*","blabla", false);
@@ -17,6 +34,8 @@ public class PaxmlUtilTest {
 		testMatch("b?","bax", false);
 		testMatch("b?","ba", true);
 		testMatch("b??x","banx", true);
+		
+		testMatch("b??x*","b\\ax", true);
 		
 	}
 	private void testMatch(String glob, String str, boolean  match){
